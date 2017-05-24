6881060a54639217d763de322763f10adee9a1e1
JasonGregory <gregoryislost@gmail.com>
2017-05-24 10:13:05 -0600

    Inital commit.

---
+++ CreatePackage.R
@@ -0,0 +1,18 @@
+library(devtools)
+library(roxygen2)
+
+setwd("~/Documents/R/Packages")
+devtools::create("dataFun")
+
+#In the DESCRIPTION file list any packages that the functions are dependent on
+#Add .R files in the "R" folder for any of the functions
+#Make sure to document appropriately
+
+devtools::document("~/Documents/R/Packages/dataFun")
+
+devtools::install("~/Documents/R/Packages/dataFun")
+
+library(dataFun)
+
+#instal from Github
+install_github("JasonGregory/dataFun")
---
+++ Explore.R
@@ -0,0 +1,12 @@
+library("devtools")
+
+install_github("JasonGregory/dataFun")
+
+
+library(dataFun)
+dat <- read.csv(system.file("extdata", "titanic.csv", package = "dataFun"))
+
+prepDescribe(dat)
+
+prepIt(dat)
+exploreIt(dat)
