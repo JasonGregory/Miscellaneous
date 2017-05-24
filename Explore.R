0ccaeb3ee22a5982286c8189a759cd8d540938bd
JasonGregory <gregoryislost@gmail.com>
2017-05-24 11:36:55 -0600

    dataFun
    Documentation on how to use the dataFun library

--- Explore.R
+++ Explore.R
@@ -1,14 +1,37 @@
 library("devtools")
-
 install_github("JasonGregory/dataFun")
 
-
 library(dataFun)
-dat <- read.csv(system.file("extdata", "titanic.csv", package = "dataFun"))
+rm(list=ls())
 
-prepDescribe(dat)
+######################
+#Data Prep Stage
+######################
+dat <- read.csv(system.file("extdata", "titanic.csv", package = "dataFun"))
+prepIt(dat)
+
+
+#convert numeric discrete variables to factor
+p.factor(dat)
+dat <- p.asFactor(dat)  #converts numeric discrete to factors
+p.structure(dat)
+
+#replace missing factor variables & remove null rows
+dat[dat==""] <- NA
+PlotMissing(dat)
+columns <- p.factor(dat); columns
+dat[columns] <- p.replaceNull(dat, columns); rm(columns)
+PlotMissing(dat)
+dat <- dat[complete.cases(dat),]
+
+#remove unique variables
+p.unique(dat)
+dat <- p.removeVariables(dat, p.unique(dat))
+p.structure(dat)
 
 prepIt(dat)
-exploreIt(dat)
 
-Blah.
+####################
+#Data Exploration Stage
+####################
+exploreIt(dat)
