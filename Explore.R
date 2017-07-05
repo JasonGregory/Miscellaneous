5a0ea604d46b56f1b878becb27d93d38dc5348fb
JasonGregory <gregoryislost@gmail.com>
2017-07-04 23:02:45 -0600

    General Update
    Updated Notes

--- Explore.R
+++ Explore.R
@@ -1,44 +1,35 @@
-#####################
-#Thought process:
-#First decide what you want to predict (frame the problem/brainstorm)
-#Think about how the data is setup and therefore needs to be sampled
-#Types of feature selection include normalization & treatment of categorical variables
-#
-#For explore it: 
-# Look to see what type of relationships there are between the dependent and independent variables
-# What relationships are the between independent variables to create stronger relationships
-# 
-#
-#
-#
-#
-#
+# Thought process ----------
+  # First decide what you want to predict (frame the problem/brainstorm)
+  # Think about how the data is setup and therefore needs to be sampled
+  # Types of feature selection include normalization & treatment of categorical variables
+
+# For explore it
+  # Look to see what type of relationships there are between the dependent and independent variables
+  # What relationships are there between independent variables to create stronger relationships
 
 
-#####################
-#Example Code
-#####################
-
+# Installing libraries ----------
 library("devtools")
 install_github("JasonGregory/dataFun")
 #devtools::install("//Co.ihc.com/sh/User/jgregor1/GitHub/dataFun")
 
+
 library(dataFun)
 rm(list=ls())
 
-######################
-#Data Prep Stage
-######################
+
+# Data Prep Stage ----------
 dat <- read.csv(system.file("extdata", "titanic.csv", package = "dataFun"))
 prepIt(dat)
 
 
-#convert numeric discrete variables to factor
+# Convert numeric discrete to factor
 p.factor(dat)
 dat <- p.asFactor(dat)  #converts numeric discrete to factors
 p.structure(dat)
 
-#replace missing factor variables & remove null rows
+
+# Replace missing factor variables & remove null rows
 dat[dat==""] <- NA
 PlotMissing(dat)
 columns <- p.factor(dat); columns
@@ -46,28 +37,28 @@ dat[columns] <- p.replaceNull(dat, columns); rm(columns)
 PlotMissing(dat)
 dat <- dat[complete.cases(dat),]
 
-#remove unique variables
+# Remove unique variables
 p.unique(dat)
 dat <- p.removeVariables(dat, p.unique(dat))
 p.structure(dat)
 
 prepIt(dat)
 
-####################
-#Data Exploration Stage
-####################
+
+# Data exploration stage ----------
+
 exploreIt(dat)
 
 
+# Other useful functions & links ----------
 
-####################
-#Other Useful Functions & links
-####################
-#similar to setdiff but goes both ways
+
+# similar to setdiff but goes both ways
 symdiff <- function( x, y) { 
   setdiff( union(x, y), intersect(x, y))
 }
-#RProfile links
+
+# RProfile links
 #https://www.r-bloggers.com/fun-with-rprofile-and-customizing-r-startup/
 #http://www.gettinggeneticsdone.com/2013/07/customize-rprofile.html
 #https://github.com/tonyfischetti/myR/blob/master/aR.profile
